39f7cdea6d98d89aae2774ca157777829cf28a55
wgwine <wgwine@gmail.com>
2016-07-08 15:39:06 -0400

    Gulp functionality

--- scratch/gulpfile.js
+++ scratch/gulpfile.js
@@ -1,15 +1,56 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var watch = require('gulp-watch');
+var nodemon = require('nodemon');
+var livereload = require('gulp-livereload');
+
 
 gulp.task('less', function () {
-  return gulp.src('./less/**/*.less')
+  return gulp.src('./frontend/less/*.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
+    .pipe(uglifycss())
+	.pipe(concat('style.min.css'))
     .pipe(gulp.dest('./public/css'))
+    .pipe(livereload())
+});
+
+gulp.watch('./frontend/less/*.less', ['less']); 
+
+gulp.task('script', function() {
+  return gulp.src(['./frontend/core.js'])
+	.pipe(concat('script.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./public/js'))
+    .pipe(livereload());
+});
+gulp.watch('./frontend/*.js', ['script']); 
+
+
+gulp.task('html', function() {
+  return gulp.src(['./frontend/index.html'])
+	.pipe(gulp.dest('./public'))    
+	.pipe(livereload());
+});
+gulp.watch('./frontend/index.html', ['html']); 
+
+
+gulp.task('watch', function() {
+  livereload.listen();
+});
+
+gulp.task('start', function () {
+  nodemon({
+    script: 'server.js'
+  , ext: 'js html'
+  })
 });
 
 gulp.task('default', [
-    'less'
+    'less','script','html','watch','start'
 ]);
---
+++ scratch/webpack.config.js
@@ -0,0 +1,7 @@
+/*module.exports = {
+  entry: "./public/core.js",
+  output: {
+    filename: "./bundles/core-bundle.js"
+  },
+  watch:true
+}*/
